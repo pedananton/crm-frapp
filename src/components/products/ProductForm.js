8ea64d2806f852b98977851e2896c22de9533e23
Anton Pedan <caravellejetta@gmail.com>
2020-11-22 20:32:16 +0200

    2032

--- src/components/products/ProductForm.js
+++ src/components/products/ProductForm.js
@@ -2,63 +2,32 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 /* eslint-disable eqeqeq */
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import { Field, Form, Formik } from 'formik';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
 function ProductForm({ product }) {
-  console.log('Form');
+  const classes = useStyles();
+
   return (
-    <>
-      <TableContainer component={Paper}>
-        <Table aria-label='simple table'>
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Quantity</TableCell>
-              <TableCell>Price</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody></TableBody>
-        </Table>
-      </TableContainer>
-      <Formik
-        initialValues={product}
-        // onSubmit={onFormSubmit}
-        // validate={validate}
-      >
-        <Form>
-          Form
-          <Field name='name' type='text' placeholder='name' />
-          <Field name='quantity' type='email' placeholder='quantity' />
-          <Field name='price' type='price'>
-            {({ field, meta }) => (
-              <div>
-                <input {...field} placeholder='+38' />
-                {meta.error ? meta.error : null}
-              </div>
-            )}
-          </Field>
-          <button type='submit'>Save</button>
-          <button
-            type='button'
-            // onClick={() => onUserDelete(user.id, onDeleteClick())}
-          >
-            Delete
-          </button>
-        </Form>
-      </Formik>
-    </>
+    <form className={classes.root} noValidate autoComplete='off'>
+      <TextField label='Product ID' variant='outlined' />
+      <TextField label='Product Name' variant='outlined' />
+      <TextField label='Product Amount' variant='outlined' />
+      <TextField label='Product Price' variant='outlined' />
+    </form>
   );
 }
 
 const mapStateToProps = (state, props) => {
-  console.log('props', props.match.params.id);
   const product =
     props.match.params.id == 'new'
       ? {
